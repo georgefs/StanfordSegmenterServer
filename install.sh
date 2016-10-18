f5ea9eeda97d27758fc720c37327753064c8c347
banyh <banyhong@gmail.com>
2016-10-18 16:53:46 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 wget http://nlp.stanford.edu/software/stanford-segmenter-2015-12-09.zip
 unzip stanford-segmenter-2015-12-09.zip
 rm -f stanford-segmenter-2015-12-09.zip
-cp stanford-segmenter-2015-12-09/* .
+cp -r stanford-segmenter-2015-12-09/* .
 rm -rf stanford-segmenter-2015-12-09
 
 unzip -o stanford-segmenter-3.6.0.jar
