3c8699ba25f938688f729038f14f4a92375856ff
george <goblin_george@gmail.com>
2017-03-09 23:15:55 +0800

    update

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ wget http://nlp.stanford.edu/software/stanford-segmenter-2015-12-09.zip
 unzip stanford-segmenter-2015-12-09.zip
 rm -f stanford-segmenter-2015-12-09.zip
 cp -r stanford-segmenter-2015-12-09/*.jar lib
+rm -rf stanford-segmenter-2015-12-09/
 
 sh make.sh
