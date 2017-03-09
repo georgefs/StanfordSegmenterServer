3664b4e4bf68ecfd3c22618ed65cd8ffb3964d46
george <goblin_george@gmail.com>
2017-03-09 23:07:21 +0800

    update

--- install.sh
+++ install.sh
@@ -3,14 +3,6 @@
 wget http://nlp.stanford.edu/software/stanford-segmenter-2015-12-09.zip
 unzip stanford-segmenter-2015-12-09.zip
 rm -f stanford-segmenter-2015-12-09.zip
-cp -r stanford-segmenter-2015-12-09/* .
-rm -rf stanford-segmenter-2015-12-09
-
-unzip -o stanford-segmenter-3.6.0.jar
-unzip -o slf4j-api.jar
-unzip -o slf4j-simple.jar
-
-mkdir -p service/impl
-mkdir -p service/endp
+cp -r stanford-segmenter-2015-12-09/*.jar lib
 
 sh make.sh
--- make.sh
+++ make.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-mkdir -p service/impl
-mkdir -p service/endp
-
-javac -cp . SegService.java
-mv -f SegService.class service/impl/
-
-javac -cp . SegServiceImpl.java
-mv -f SegServiceImpl.class service/impl/
-
-javac SegServicePublisher.java
-mv -f SegServicePublisher.class service/endp/
+javac -cp lib/*:. -d . src/*.java
